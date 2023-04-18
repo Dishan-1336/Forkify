84e52ff54a77f99f3ddd7036f3df08b9cfceed42
Dishan <Dishan.Ahmed@brainstation-23.com>
2023-04-18 15:02:48 +0600

    completed searching and showing result to console

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
-export const KEY = '11d51632-65fe-44af-bf29-5dcd1e3ceca6';
+export const KEY = 'c03680ea-68e6-458f-8a60-19a2af5b58d1';
